3e230c439c203e4061f4dc0252199194bc796ba1
Tigran Frangyan <ftigran1@gmail.com>
2022-01-30 18:37:56 +0300

    Updates

--- src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import commitsReducer from "../components/commitsSlice";
-
-export const store = configureStore({
-    reducer: {
-        commits: commitsReducer,
-    },
-});
